5bc8a6628030576673fff28e9d6cf7b39cafccd5
giorgi8798 <154001339+giorgi8798@users.noreply.github.com>
2024-03-13 20:19:01 +0400

    .

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ cart.addEventListener("click", () => {
   }
   display = dropCart.style.display;
 });
+
+const bigPhoto = document.querySelector(".bigPhoto");
+const smalPhoto = document.querySelectorAll(".img1");
+
+console.dir(bigPhoto.src);
+console.log(smalPhoto);
+console.log(smalPhoto[2]);
+
+function changeUrl(x) {
+  bigPhoto.src = smalPhoto[x].src;
+}
+smalPhoto[0].addEventListener("click", () => changeUrl(0));
+smalPhoto[1].addEventListener("click", () => changeUrl(1));
+smalPhoto[2].addEventListener("click", () => changeUrl(2));
+smalPhoto[3].addEventListener("click", () => changeUrl(3));
